645828b8803faf880b02bcab2153e5c897a80e32
Matt Harris <matrharr@gmail.com>
2016-09-25 13:32:40 -0700

    add route logic

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,7 +1,10 @@
 class PaintingsController < ApplicationController
 
   def today
-    p "test"
+    @today_painting = Painting.find(1)
+      if request.xhr?
+        render json: @today_painting
+      end
   end
 
 
